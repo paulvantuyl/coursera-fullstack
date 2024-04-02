4a91e730e8c0f706e0d69e960ae4f2c78aa10e13
Paul <paulvantuyl@users.noreply.github.com>
2024-04-01 17:42:33 -0700

    Add in ternary operator for control flow lab

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -44,3 +44,9 @@ switch (userType) {
 }
 
 console.log("User Category:", userCategory)
+
+let isAuthenticated = true;
+
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+
+console.log("Authentication Status:", authenticationStatus);
