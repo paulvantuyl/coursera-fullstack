24ed4d6de3f88fdf5fb93ea246b46479ac932092
Paul <paulvantuyl@users.noreply.github.com>
2024-04-19 19:55:59 -0700

    Finalize ToDo list Lab

--- toDoList/todo_list.js
+++ toDoList/todo_list.js
@@ -2,6 +2,7 @@ const taskInput = document.getElementById('taskInput');
 const addTaskBtn = document.getElementById('addTaskBtn');
 const taskList = document.getElementById('taskList');
 const clearCompletedBtn = document.getElementById('clearCompletedBtn');
+const clearAllBtn = document.getElementById('clearAllBtn');
 
 let tasks = [];
 
@@ -35,7 +36,18 @@ function clearCompletedTasks() {
     displayTasks();
 }
 
+// Get the length of the array
+// Tell the slice to delete that number
+function clearAllTasks() {
+    const indexLength = tasks.length;
+    console.log(indexLength);
+    tasks = tasks.splice(indexLength);
+    console.log(tasks);
+    displayTasks();
+}
+
 addTaskBtn.addEventListener('click', addTask);
 clearCompletedBtn.addEventListener('click', clearCompletedTasks);
+clearAllBtn.addEventListener('click', clearAllTasks);
 
 displayTasks();
