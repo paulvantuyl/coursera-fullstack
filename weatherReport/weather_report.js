6b44aed372d368f170c3193e13d31c28eab9f41d
Paul <paulvantuyl@users.noreply.github.com>
2024-04-25 19:49:54 -0700

    Lab using fetch() to call weather data

---
+++ weatherReport/weather_report.js
@@ -0,0 +1,23 @@
+function showWeatherDetails(event) {
+    event.preventDefault();
+
+    const city = document.getElementById('city').value;
+    const apiKey = '92dd4e60316b73f3bd6afa5f8d5fd21e';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                                     <p>Temperature: ${data.main.temp} &#8451;</p>
+                                     <p>Weather: ${data.weather[0].description}</p>`;
+        })
+        .catch(error => {
+            console.error('Error fetching weather:', error);
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`
+        });
+}
+
+document.getElementById('weatherForm').addEventListener('submit', showWeatherDetails);
